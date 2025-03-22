cf2824713f2f0b6f0248fb9fac2e706d7dfa2079
Adrian Kotlinski <kotlinski95@gmail.com>
2025-03-22 12:58:07 +0100

    fix(T4-patterns): add missing header structure to header template part

--- wp-content/themes/kotlinskidev/patterns/header.php
+++ wp-content/themes/kotlinskidev/patterns/header.php
@@ -6,5 +6,24 @@
  * Categories: header, kotlinskidev/header, themeslug/custom
  */
 
- echo do_shortcode('[navigation]');
 ?>
+
+<!-- wp:group {"metadata":{"categories":["kotlinskidev"],"patternName":"kotlinskidev/header-default","name":"Header Default"},"tagName":"header","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"100%"}} -->
+<header class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0px","bottom":"0px","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"constrained","contentSize":"1180px"}} -->
+    <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0px;padding-right:var(--wp--preset--spacing--40);padding-bottom:0px;padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+        <div class="wp-block-group">
+            <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group" style="margin-top:0;margin-bottom:0">
+                <!-- wp:site-logo {"width":85,"shouldSyncIcon":false} /-->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:shortcode -->
+            [navigation]
+            <!-- /wp:shortcode -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</header>
+<!-- /wp:group -->
