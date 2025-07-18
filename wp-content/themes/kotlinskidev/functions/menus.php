9dc1e57ea3ffb602c5599b3a4872ba0d682fd029
Adrian Kotlinski <kotlinski95@gmail.com>
2025-07-18 12:24:25 +0200

    fix(T4): fix mobile hamburger menu styles and UI, add social navigation menu

--- wp-content/themes/kotlinskidev/functions/menus.php
+++ wp-content/themes/kotlinskidev/functions/menus.php
@@ -4,7 +4,8 @@ function theme_slug_register_menus()
     register_nav_menus(
         array(
             'primary' => __('Header menu', 'theme_slug'),
-            'mobile'  => __('Mobile Menu', 'theme_slug')
+            'mobile'  => __('Mobile Menu', 'theme_slug'),
+            'social'  => __('Social Media Menu', 'theme_slug')
         )
     );
 }
