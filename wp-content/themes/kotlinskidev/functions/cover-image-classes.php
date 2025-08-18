6d77e359bc154fe68cd227eba1713d14865064db
Adrian Kotlinski <kotlinski95@gmail.com>
2025-08-18 10:15:13 +0200

    fix(T5): fix lazy laoding of core/image block

--- wp-content/themes/kotlinskidev/functions/cover-image-classes.php
+++ wp-content/themes/kotlinskidev/functions/cover-image-classes.php
@@ -4,19 +4,19 @@
  */
 function add_cover_image_classes($output, $block)
 {
-    // Only process cover blocks
-    if ($block['blockName'] !== 'core/cover') {
+    // Only process cover and image blocks
+    if ($block['blockName'] !== 'core/cover' && $block['blockName'] !== 'core/image') {
         return $output;
     }
 
-    // Check if this specific cover block should skip lazy loading
+    // Check if this specific block should skip lazy loading
     $skip_lazy = isset($block['attrs']['kotlinskidevSkipLazy']) && $block['attrs']['kotlinskidevSkipLazy'];
     
     if ($skip_lazy) {
         // Get the configurable class name from theme options
         $lazy_class = get_theme_mod('kotlinskidev_lazy_loading_class', 'skip-lazy');
         
-        // Add the class to exclude from lazy loading to any img tag in the cover block
+        // Add the class to exclude from lazy loading to any img tag
         $output = preg_replace(
             '/(<img[^>]*class="[^"]*)(")/',
             '$1 ' . esc_attr($lazy_class) . '$2',
